dfc76bca880a4681f91ea045d517053491f3ffef
fslepsot <ftlepsot@student.apc.edu.ph>
2025-01-13 10:21:19 +0800

    edited vue.js

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,22 +1,25 @@
-new Vue({
-    el: '#app',
-    data: {
-      feedback: {
-        name: '',
-        email: '',
-        message: ''
+document.addEventListener('DOMContentLoaded', function () {
+    new Vue({
+      el: '#app',
+      data: {
+        feedback: {
+          name: '',
+          email: '',
+          message: ''
+        },
+        showFeedbackModal: false
       },
-      showFeedbackModal: false
-    },
-    methods: {
-      submitFeedback() {
-        if (this.feedback.name && this.feedback.email && this.feedback.message) {
-          this.showFeedbackModal = true;
-          this.feedback = { name: '', email: '', message: '' };
-        } else {
-          alert('Please fill out all fields.');
+      methods: {
+        submitFeedback() {
+          if (this.feedback.name && this.feedback.email && this.feedback.message) {
+            console.log('Feedback Submitted:', this.feedback);
+            this.showFeedbackModal = true;
+            this.feedback = { name: '', email: '', message: '' };
+          } else {
+            alert('Please fill out all fields.');
+          }
         }
       }
-    }
+    });
   });
   
